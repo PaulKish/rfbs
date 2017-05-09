3d7db72d3d7ef62ddc999f4afa501cd4d3174b9e
Paul Ngumii <paulngumii@gmail.com>
2017-05-09 11:23:54 +0300

    Fix for frontend of resources

--- views/site/_resource.php
+++ views/site/_resource.php
@@ -2,11 +2,17 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 ?>
-<div class="report">
-    <h3><?= Html::encode($model->title) ?></h3>
-    <small><i class="fa fa-clock-o"></i> posted <?= date('F j, Y, g:i a',strtotime($model->date)) ?></small>
+<div class="report well">
+    <h3><?= Html::encode($model->title) ?> <small>under <?= $model->category ?></small></h3>
+    <small>
+    	<i class="fa fa-clock-o"></i> posted <?= date('F j, Y, g:i a',strtotime($model->date)) ?>
+    </small>
+
+    <hr>
+    <p><?= $model->content ?></p>
+
     <br>
-    <br>
-	<?= Html::a('<i class="fa fa-download"></i> Download',Url::to($model->getUploadUrl('upload')),['class'=>'btn btn-primary']) ?> 
-	<hr> 
+    <?php if(isset($model->upload)): ?>
+		<?= Html::a('<i class="fa fa-download"></i> Download',Url::to($model->getUploadUrl('upload')),['class'=>'btn btn-primary']) ?> 
+	<?php endif; ?>
 </div>
