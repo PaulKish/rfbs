599f94169eea991aafd9823e232b44a7578c2dae
Paul Ngumii <paulngumii@gmail.com>
2017-05-10 15:48:06 +0300

    Test color change

--- assets/MainAsset.php
+++ assets/MainAsset.php
@@ -18,7 +18,7 @@ class MainAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/bootstrap-simplex.min.css',
+        'css/bootstrap-simplex.beautified.css',
         'https://cdnjs.cloudflare.com/ajax/libs/TableExport/3.2.5/css/tableexport.min.css',
         'css/site.css',
     ];
