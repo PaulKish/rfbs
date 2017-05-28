d10b9ec9c42b34d5e327afb004724ccd737a2bec
Paul Ngumii <paulngumii@gmail.com>
2017-05-28 22:57:32 +0300

    Add instructions to email

--- views/user/mail/new_password.php
+++ views/user/mail/new_password.php
@@ -10,6 +10,7 @@
  */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /**
  * @var dektrium\user\Module          $module
@@ -24,7 +25,7 @@ use yii\helpers\Html;
 
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'Your account on {0} has a new password', Yii::$app->name) ?>.
-    <p>Website Link: <a href="http://rfbs.ratin.net">http://rfbs.ratin.net</a></p>
+    <p>Website Link: <?= Html::a(Url::home(true),Url::home(true)); ?></a></p>
     <p>Your username: <strong><?= $user->username ?></strong></p>
     <?= Yii::t('user', 'We have generated a password for you') ?>: <strong><?= $user->password ?></strong>
 </p>
--- views/user/mail/welcome.php
+++ views/user/mail/welcome.php
@@ -10,6 +10,7 @@
  */
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 /**
  * @var dektrium\user\Module $module
@@ -25,7 +26,7 @@ use yii\helpers\Html;
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'Your account on {0} has been created', Yii::$app->name) ?>.
     <?php if ($showPassword || $module->enableGeneratingPassword): ?>
-        <p>Website Link: <a href="http://rfbs.ratin.net">http://rfbs.ratin.net</a></p>
+        <p>Website Link: <?= Html::a(Url::home(true),Url::home(true)); ?></a></p>
         <p>Your username: <strong><?= $user->username ?></strong></p>
         <p><?= Yii::t('user', 'We have generated a password for you') ?>: <strong><?= $user->password ?></strong></p>
     <?php endif ?>
