767ac8ef3ad77e9808bd16f1e7cef977d71c8c9c
Paul Ngumii <paulngumii@gmail.com>
2017-05-28 22:46:57 +0300

    Add details to account

--- views/user/mail/welcome.php
+++ views/user/mail/welcome.php
@@ -25,7 +25,9 @@ use yii\helpers\Html;
 <p style="font-family: 'Helvetica Neue', 'Helvetica', Helvetica, Arial, sans-serif; font-size: 14px; line-height: 1.6; font-weight: normal; margin: 0 0 10px; padding: 0;">
     <?= Yii::t('user', 'Your account on {0} has been created', Yii::$app->name) ?>.
     <?php if ($showPassword || $module->enableGeneratingPassword): ?>
-        <?= Yii::t('user', 'We have generated a password for you') ?>: <strong><?= $user->password ?></strong>
+        <p>Website Link: <a href="http://rfbs.ratin.net">http://rfbs.ratin.net</a></p>
+        <p>Your username: <strong><?= $user->username ?></strong></p>
+        <p><?= Yii::t('user', 'We have generated a password for you') ?>: <strong><?= $user->password ?></strong></p>
     <?php endif ?>
 
 </p>
